b8c4404ec0dae0719c2e46addc5c0ea22d4c7cc9
israelnp <israelpereira1984@gmail.com>
2024-06-18 13:09:29 -0300

    update sg vpn

--- vpn-server/sg.tf
+++ vpn-server/sg.tf
@@ -59,6 +59,13 @@ resource "aws_security_group" "this" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 17375
+    to_port     = 17375
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
